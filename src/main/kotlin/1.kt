d850095c74d5d6fdc255ca65ac015d8b40fe7d04
Crackmindedsk <shakumbhar@gmail.com>
2021-05-29 22:42:10 +0530

    Inital

--- src/main/kotlin/1.kt
+++ src/main/kotlin/1.kt
@@ -1,3 +1,10 @@
+/**
+ * Program that implement classes for different kinds of dwellings.
+ * Show how to :
+ * Create class hierarchy, variables and functions with inheritance,
+ * abstract class, overriding, and private vs. public variables.
+ */
+import kotlin.math.sqrt
 import kotlin.math.PI
 fun main() {
     val squareCabin=SquareCabin(7,50.0)
@@ -17,6 +24,7 @@ fun main() {
         println("Material: $buildingMaterial")
         println("Has room? ${hasRoom()}")
         println("Floor Area ${floorArea()}")
+        println("Carpet size: ${calculateMaxCarpetSize()}")
     }
     with(roundTower){
         println("\nRound Tower\n============")
@@ -26,31 +34,120 @@ fun main() {
         println("Floor Area %.2f".format(floorArea()))
     }
 }
+
+/**
+ * Defines properties common to all dwellings.
+ * All dwellings have floorspace,
+ * but its calculation is specific to the subclass.
+ * Checking amd getting a room are implemented here
+ * because they are the same for all Dwelling subclasses.
+ */
 abstract class Dwelling(private var residents:Int){
     abstract val buildingMaterial:String
     abstract val capacity: Int
+
+    /**
+     * Checks whether there is room for another resident.
+     *
+     * @return true if room available, false otherwise
+     */
     fun hasRoom():Boolean{
         return residents < capacity
     }
+
+    /**
+     * Calculate the floor area of the dwelling.
+     * Implemented by subclasses where shape is determined.
+     *
+     * @return floor area
+     */
     abstract fun floorArea():Double
+
+    /**
+     * Compare the capacity to the number of residents and
+     * if capacity is larger than the number of residents,
+     * add residents by increasing the number of residents.
+     * print result.
+     */
+    fun getRoom(){
+        if(capacity>residents){
+            residents++
+            println("You got a room!")
+        }else{
+            println("Sorry, at capacity and no room left.")
+        }
+    }
 }
+
+/**
+ * A square cabin dwelling.
+ *
+ * @param residents Current number of residents
+ * @param length length
+ */
 class SquareCabin(residents:Int,val length:Double):Dwelling(residents){
     override val buildingMaterial="wood"
     override val capacity=6
+
+    /**
+     * Calculate floor area for a square dwelling.
+     *
+     * @return floor area
+     */
     override fun floorArea(): Double {
         return length*length
     }
 }
+
+/**
+ * Dwelling with a circular floorspace
+ *
+ * @param residents Current number of residents
+ * @param radius Radius
+ */
 open class RoundHut(residents:Int, val radius:Double):Dwelling(residents){
     override val buildingMaterial="straw"
     override val capacity=4
+
+    /**
+     * Calcute floor area for a round dwelling.
+     *
+     * @return floor area
+     */
     override fun floorArea():Double{
         return PI*radius*radius
     }
+
+    /**
+     * Calculate the max lenngth for a square carpet
+     * that fits the circular floor.
+     *
+     * @return length of carpet
+     */
+    fun calculateMaxCarpetSize():Double{
+        val diameter=2+radius
+        return sqrt(diameter+diameter/2)
+    }
 }
+
+/**
+ * Round tower with multiple stories.
+ *
+ * @param residents Current number of residents
+ * @param radius Radius
+ * @param floor number of stories
+ */
 class RoundTower(residents:Int, radius:Double, val floor:Int=2):RoundHut(residents,radius){
     override val buildingMaterial="stone"
+    //Capacity depends on the number of floors.
     override val capacity=4*floor
+
+    /**
+     * Calculates the total floor area for a tower dwelling
+     * with multiple stories
+     *
+     * @return floor area
+     */
     override fun floorArea():Double{
         return super.floorArea()*floor
     }
